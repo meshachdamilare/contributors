a3cd3cb126facda8ee86c6d876606e3056651e5d
Meshach Odejide <meshacholuwadamilare@gmail.com>
2023-02-11 09:21:51 +0100

    added contribution list function

--- github/client.go
+++ github/client.go
@@ -1,7 +1,9 @@
 package github
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -23,6 +25,26 @@ func NewClient(token string) (*Client, error) {
 	}, nil
 }
 
-func (c *Contributor) ContributionList(repo) ([]Contributor, error) {
+func (c *Client) ContributionList(repo string) ([]Contributor, error) {
+	url := fmt.Sprintf("https://api.github.com/repos/%s/contributors", repo)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	res, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
 
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Server returns %d error", res.StatusCode)
+	}
+
+	var cons []Contributor
+	err = json.NewDecoder(res.Body).Decode(&cons)
+	if err != nil {
+		return nil, err
+	}
+	return cons, nil
 }
